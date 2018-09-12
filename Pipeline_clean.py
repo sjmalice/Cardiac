1995fbbace1d745a32e6fafc8f21234a276ee795
Alex Baransky <40646181+alex-baransky@users.noreply.github.com>
2018-09-12 17:05:32 -0400

    add alex version of pipeline_clean

---
+++ Pipeline_clean.py
@@ -0,0 +1,70 @@
+%load_ext autoreload
+%autoreload 2
+import pandas as pd
+import numpy as np
+from data_merge import *
+from Clean_Fun import *
+
+# NOTE have to use remove_invalid_rows() inside ALex's function,
+# before we remove patient name
+impute_na=9999
+# %% Load dataset
+
+live_path='Data/Cardiac Program_M.xlsx'
+archive_path='Data/Cardiac Program_Archive.xlsx'
+live_sheet_pkl='pickle_jar/live_sheets.pkl'
+archive_sheet_pkl='pickle_jar/archive_sheets.pkl'
+datecol_pkl='pickle_jar/datecols.pkl'
+df_dict=pairwise_sheet_merge(live_path, archive_path,
+    live_sheet_pkl, archive_sheet_pkl, datecol_pkl)
+df_dict.keys()
+for key in df_dict.keys():
+    print(key+":\n")
+    print(df_dict[key].columns)
+
+
+# %% test patients, determing Response Value
+
+# NOTE have to remove invalid rows
+
+df = determine_outcome_train_test(df)
+train_df,test_df=train_test_split_sg(df)
+df=train_df.copy() # for now
+del test_df
+
+# %% Clean effusion rate
+
+df['ef']=df['ef'].apply(lambda x: clean_EF_rows(x))
+
+# Clean Blood Pressure rows
+
+df['diastolic']=df.apply(lambda row: clean_diastolic_columns(
+    row['diastolic'],row['resting_bp'],col_type='di'),axis=1)
+df['systolic']=df.apply(lambda row: clean_diastolic_columns(
+    row['systolic'],row['resting_bp'],col_type='sys'),axis=1)
+
+# Dummify the diagnoses
+uniq_diag=find_unique_diag(df.diagnosis_1)
+dummy_df_diag=dummify_diagnoses(df,uniq_diag,diagnosis_col='diagnosis_1')
+df.drop('diagnosis_1',axis=1,inplace=True)
+df=df.merge(dummy_df_diag,on='enrollId',how="inner")
+
+# Clean Meds and aicd
+# acute or chronic
+
+med_aicd_clean(df,'ace', 0)
+med_aicd_clean(df,'bb', 0)
+med_aicd_clean(df,'diuretics', 0)
+med_aicd_clean(df,'anticoagulant', 0)
+med_aicd_clean(df,'ionotropes', 0)
+med_aicd_clean(df,'aicd', 0)
+
+weight_dur_age_clean(df,dur_na=-999999,age_na=-99.,weight_perc_cutoff=0.2)
+remove_invalid_rows(df)
+# %%
+pd.set_option('display.max_columns', 60)
+df.drop_duplicates(inplace=True)
+df.reset_index(inplace=True)
+df.drop(columns=['index'], inplace=True)
+df
+test_df
