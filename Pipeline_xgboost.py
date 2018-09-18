6aff98c67bae9c440269fbffccedceb69138490c
Geoghan <geoghan12@gmail.com>
2018-09-18 18:20:38 -0400

    response df

--- Pipeline_xgboost.py
+++ Pipeline_xgboost.py
@@ -17,7 +17,7 @@ df=meta_clean(df)
 df=df[df['outcome']!=2]
 # %%
 # load the columns I want to keep while modelling
-df.columns
+# df.columns
 column_dict=read_pkl('Models/model_columns2.pkl')
 keep_cols=column_dict['keep_cols']
 pat_cols=column_dict['pat_cols']
@@ -37,28 +37,35 @@ df[log_cols].isnull().sum()
 
 for col in log_cols:
     df[col]=np.log1p(df[col]+1)
-    plt.hist(train(df)[col].dropna())
-    plt.title(str(col))
-    plt.show()
+    # plt.hist(train(df)[col].dropna())
+    # plt.title(str(col))
+    # plt.show()
+
+# write_pkl(df,'model_data.pkl')
+
 # %%
 
 from numpy import loadtxt
 from xgboost import XGBClassifier
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split, GridSearchCV,cross_val_score
 from sklearn.metrics import accuracy_score, precision_score, roc_curve
 
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
 
 # %%
 x=train(df)[keep_cols].drop('outcome',axis=1).dropna()
 y=train(df)[keep_cols].dropna()['outcome']
 
-x_train, x_test, y_train, y_test = train_test_split(x,y)
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.25,random_state=43)
 
 param_grid={'learning_rate':np.linspace(0.01,0.99,20),'max_depth':np.arange(3,10)}
 model = XGBClassifier() #
 model=GridSearchCV(estimator=model, param_grid= param_grid, scoring='precision', cv=5, return_train_score=True) #param_grid=grid_param,
 model.fit(x_train,y_train)
+model.cv_results_
+cross_val_score(model,x_train,y_train,cv=5)
+
+dir(model)
 print(model.best_score_)
 print(model.best_params_)
 # model.fit(x_train, y_train)
@@ -76,13 +83,15 @@ cnf_matrix = confusion_matrix(y_test, predictions)
 
 prediction=model.predict_proba(x_test)
 # prediction
-low_threshold_pred=pd.Series(prediction[:,1]).apply(lambda x: 0 if x<0.60 else 1)
+low_threshold_pred=pd.Series(prediction[:,1]).apply(lambda x: 0 if x<0.75 else 1)
 
 precision_score(y_test,low_threshold_pred)
 cnf_matrix = confusion_matrix(y_test, low_threshold_pred)
 cnf_matrix
-from sklearn.metrics import accuracy_score
+
 accuracy = accuracy_score(y_test, low_threshold_pred)
 print("Accuracy: %.2f%%" % (accuracy * 100.0))
 
+## I want something like this, maybe a dataframe with predictions, probabilites
+predictions=para_search.predict(test(df)[keep_cols].drop('outcome',axis=1).dropna())
 # roc_curve(y_test,prediction[:,1])
